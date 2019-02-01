b3a84d1b70ca5be4c077eb6ea7cb6af348e4f80e
imac08 <a@b.com>
2019-02-01 15:49:16 -0500

    Fix Sign UP

--- routes/routes.js
+++ routes/routes.js
@@ -313,6 +313,7 @@ module.exports = function (app, passport) {
 
     app.get('/deleteRow', isLoggedIn, function (req, res) {
         del_recov("Delete", "Deletion failed!", "/userHome", req, res);
+        console.log();
     });
 
     app.get('/recoverRow', isLoggedIn, function (req, res) {
